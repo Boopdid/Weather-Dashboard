63f3ffb23a1e091bb7b30ed87b0bcf7a82ca6b77
Peter Vrutneski <pvrutneski@gmail.com>
2022-04-05 16:38:46 -0600

    add 5 day forecast functions andthe functions to save cities

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,62 @@ var inputEl = $("#input");
 var searchBtn = $("#btn");
 var savedCities = $("#savedCities");
 
+function getSavedCities() {
+  var storedSavedCityNames = localStorage.getItem("savedCityNames");
+  var savedCityNames = [];
+  if (storedSavedCityNames) {
+    savedCityNames = JSON.parse(storedSavedCityNames);
+  }
+  return savedCityNames;
+}
+
+function saveCityName(cityName) {
+  var savedCityNames = getSavedCities();
+  // console.log("Saved Park Names: ", savedCityNames);
+  var alreadySaved = savedCityNames.includes(cityName);
+  if (!alreadySaved) {
+    savedCityNames = savedCityNames.concat(cityName);
+    localStorage.setItem("savedCityNames", JSON.stringify(savedCityNames));
+  }
+}
+
+function populateSavedContent() {
+  var savedCityNames = getSavedCities();
+  if (savedCityNames.length !== 0) {
+    $("#savedCities").empty();
+    savedCityNames.forEach(function (cityName) {
+      var cityBtn = $("<button>");
+      cityBtn.text(cityName);
+      cityBtn.attr("id", cityName);
+      cityBtn.addClass("button is-fullwidth m-1");
+      cityBtn.addClass("wrapButtonText");
+      cityBtn.addClass("savedSearch");
+      $("#savedCities").append(cityBtn);
+    });
+  }
+  $(".savedSearch").on("click", savedCitySearch);
+}
+
+function fiveDayFunction(fiveDayForecast) {
+  for (let i = 0; i < fiveDayForecast.length; i++) {
+    var data = fiveDayForecast[i];
+    var date = moment.unix(data.dt).format("MM/DD/YYYY");
+    var icon = data.weather[0].icon;
+    var dayTemp = data.temp.max;
+    var dayWind = data.wind_speed;
+    var dayHumidity = data.humidity;
+    $(`#day${i + 1}`).text(date);
+    $(`#icon${i + 1}`).attr(
+      "src",
+      `http://openweathermap.org/img/wn/${icon}.png`
+    );
+    $(`#temp${i + 1}`).text(dayTemp);
+    $(`#wind${i + 1}`).text(dayWind);
+    $(`#humidity${i + 1}`).text(dayHumidity);
+  }
+}
 function oneCall(lat, lon) {
-  var oneCallApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+  var oneCallApi = `https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=${lat}&lon=${lon}&appid=${apiKey}`;
   $.get(oneCallApi, function (data) {
     console.log(data);
     var uvIndex = data.current.uvi;
@@ -19,6 +73,9 @@ function oneCall(lat, lon) {
     } else {
       $("#uvIndex").attr("class", "vhighRisk");
     }
+    var dailyforecast = data.daily.slice(1, 6);
+    console.log(dailyforecast);
+    fiveDayFunction(dailyforecast);
   });
 }
 function getWeather(searchedCity) {
@@ -26,6 +83,7 @@ function getWeather(searchedCity) {
   $.get(currentWeatherApi, function (data) {
     console.log(data);
     var currentCity = data.name;
+    var currentDate = moment().format("(MM/DD/YYYY)");
     var currentIcon = data.weather[0].icon;
     var currentTemp = data.main.temp;
     var currentWind = data.wind.speed;
@@ -34,6 +92,7 @@ function getWeather(searchedCity) {
     var lon = data.coord.lon;
     oneCall(lat, lon);
     $("#city").text(currentCity);
+    $("#currentDate").text(currentDate);
     $("#mainIcon").attr(
       "src",
       `http://openweathermap.org/img/wn/${currentIcon}@2x.png`
@@ -41,10 +100,20 @@ function getWeather(searchedCity) {
     $("#temp").text(currentTemp);
     $("#wind").text(currentWind);
     $("#humidity").text(currentHumidity);
+    saveCityName(currentCity);
+    populateSavedContent();
   });
+}
+
+function savedCitySearch(event) {
+  event.preventDefault();
+  chosenCity = event.target.innerText;
+  getWeather(chosenCity);
 }
 
 searchBtn.click(function () {
   var searchedCity = inputEl.val();
   getWeather(searchedCity);
 });
+
+populateSavedContent();
