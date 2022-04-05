2849e776431cba02bc33815227014e25f1a797bf
Peter Vrutneski <pvrutneski@gmail.com>
2022-04-04 23:04:47 -0600

    added javacript to call api's and fetch the current weather for searched city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,50 @@
 // Global variables
 var apiKey = "1bb5db5b652ed01ed43e82a79df4ebd1";
-var searchWeather = 
+var inputEl = $("#input");
+var searchBtn = $("#btn");
+var savedCities = $("#savedCities");
+
+function oneCall(lat, lon) {
+  var oneCallApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+  $.get(oneCallApi, function (data) {
+    console.log(data);
+    var uvIndex = data.current.uvi;
+    $("#uvIndex").text(uvIndex);
+    if (uvIndex < 3) {
+      $("#uvIndex").attr("class", "minDanger");
+    } else if (uvIndex < 6) {
+      $("#uvIndex").attr("class", "lowRisk");
+    } else if (uvIndex < 11) {
+      $("#uvIndex").attr("class", "highRisk");
+    } else {
+      $("#uvIndex").attr("class", "vhighRisk");
+    }
+  });
+}
+function getWeather(searchedCity) {
+  var currentWeatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&units=imperial&appid=${apiKey}`;
+  $.get(currentWeatherApi, function (data) {
+    console.log(data);
+    var currentCity = data.name;
+    var currentIcon = data.weather[0].icon;
+    var currentTemp = data.main.temp;
+    var currentWind = data.wind.speed;
+    var currentHumidity = data.main.humidity;
+    var lat = data.coord.lat;
+    var lon = data.coord.lon;
+    oneCall(lat, lon);
+    $("#city").text(currentCity);
+    $("#mainIcon").attr(
+      "src",
+      `http://openweathermap.org/img/wn/${currentIcon}@2x.png`
+    );
+    $("#temp").text(currentTemp);
+    $("#wind").text(currentWind);
+    $("#humidity").text(currentHumidity);
+  });
+}
+
+searchBtn.click(function () {
+  var searchedCity = inputEl.val();
+  getWeather(searchedCity);
+});
